9288136e1ce19102c1502c2ec97c5897e1ca41ab
wuttisakk <74964474+wuttisakk@users.noreply.github.com>
2021-02-18 22:28:01 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,60 +1,88 @@
-import webbrowser
 import sys
 import subprocess
 import PySimpleGUI as sg
+import webbrowser
+
 
 sg.set_options()
 sg.theme('Reddit')
-layout = [[sg.Text("  Video Streaming  ", text_color=('red'), size=(28, 1), justification='center', font=("Helvetica", 48), relief=sg.RELIEF_RIDGE)],
-          [sg.Text("")],
+layout = [[sg.Text("  Video Streaming  ",
+                   text_color=('red'),
+                   size=(20, 1),
+                   justification='center',
+                   font=("Helvetica", 48),
+                   relief=sg.RELIEF_RIDGE)],
           [sg.Text("")],
           [sg.Text("")],
           [sg.Frame('',[
-             [sg.Text("")],
-             [sg.Text("Streaming Video ",
-                      size=(50, 1),
+             [sg.Text("Streaming")],
+             [sg.Text("Streaming ",
+                      size=(32, 1),
                       text_color=('blue'),
                       font=("Helvetica", 24)),
-              sg.Button("Streaming",
+              sg.Button("stream",
                         button_color=('white', 'blue'),
-                        size=(8, 1),
+                        size=(6, 1),
                         font=("Helvetica", 20))],
              [sg.Text("Record Video",
-                      size=(50, 1),
+                      size=(32, 1),
                       text_color=('blue'),
                       font=("Helvetica", 24)),
-              sg.Button("Record",
+              sg.Button("record",
                         button_color=('white', 'blue'),
-                        size=(8, 1),
+                        size=(6, 1),
                         font=("Helvetica", 20))],
+             [sg.Text("")],
+          ])],
+          [sg.Frame('Live Video',[
+             [sg.Text("")],
+             [sg.Text("Live Video",
+                      size=(25, 1),
+                      text_color=('blue'),
+                      font=("Helvetica", 24)),
+              sg.Text('Select Video',
+                      font=("Helvetica", 16)),
+              sg.Button("Select",
+                        button_color=('white', 'blue'),
+                        size=(10, 1),
+                        font=("Helvetica", 12))],
+             [sg.Text("", size=(25, 1),
+                      text_color=('blue'),
+                      font=("Helvetica", 24)),
+              sg.Text('Start Stream',
+                      font=("Helvetica", 16)),
+              sg.Button("LIVE",
+                        button_color=('white', 'blue'),
+                        size=(10, 1),
+                        font=("Helvetica", 12))],
              [sg.Text("")],
           ])],
           [sg.Text("")],
           [sg.Text("")],
-          [sg.Text("")],
-          [sg.Text("")],
           [sg.Text("Exit From Video Streaming",
-                   size=(50, 1), text_color=('blue'),
+                   size=(32, 1),
+                   text_color=('blue'),
                    font=("Helvetica", 24)),
-           sg.Button("Exit",
+           sg.Button("EXIT",
                      button_color=('white', 'red'),
                      size=(6, 1),
                      font=("Helvetica", 20))],
 
          ]
-
 window = sg.Window("Video Streaming", layout)
-
 while True:
     event, values = window.read()
-    if event == "Record":
-       subprocess.Popen('python record.py', shell=True)
-
-    if event == "Streaming":
-       subprocess.Popen('python project.py', shell=True)
-       webbrowser.open('http://127.0.0.1:5000/')
-
-    if event == "Exit" or event == sg.WIN_CLOSED:
+    if event == "stream":
+       subprocess.Popen('python /home/pi/ProjectOS-Video-Streaming/project.py', shell=True)
+       webbrowser.open('http://10.0.2.15/', new=2)
+       continue
+    if event == "record":
+        subprocess.Popen('python /home/pi/ProjectOS-Video-Streaming/videoRec.py', shell=True)
+    if event == "Select":
+        subprocess.Popen('python /home/pi/ProjectOS-Video-Streaming/project_video.py', shell=True)
+    if event == "LIVE":
+       webbrowser.open('http://10.0.2.15/', new=2)
+    if event == "OK" or event == sg.WIN_CLOSED:
         break
 
 window.close()
--- project_video.py
+++ project_video.py
@@ -1,13 +1,26 @@
 from flask import Flask, render_template, Response
 import cv2
+import PySimpleGUI as sg
 
 app = Flask(__name__)
+sg.theme('Reddit')
+layout = [[sg.Text("Your Video", size=(25,1),justification='right'),sg.InputText('Defalut Folder'), sg.FileBrowse(), sg.OK()],
+          [sg.Text("",size=(83,1)),sg.Button("Exit", button_color=('white', 'red'), size=(4, 1), font=("Helvetica", 8))]
+          ]
+window = sg.Window("Select Video", layout)
 
-camera = cv2.VideoCapture(0)
+while True:
+    event, values = window.read()
+    if event == "Exit" or event == sg.WIN_CLOSED:
+       break
+   # print("This is : ", values[0])
+
+print("This is : ", values[0])        
+cap = cv2.VideoCapture(values[0])
 
 def gen_frames():
     while True:
-        success, frame = camera.read()
+        success, frame = cap.read()
         if not success:
             break
         else:
@@ -17,6 +30,7 @@ def gen_frames():
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
+# Web Streaming With Flask
 @app.route('/video_feed')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
